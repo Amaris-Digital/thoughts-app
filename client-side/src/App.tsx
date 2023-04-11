8d2b46f430988e7c9a67d7c236713f5281e2087c
cindykandie <cindyjk6@gmail.com>
2023-04-11 09:18:26 +0300

    Repo: Set up React App with Vite ts

---
+++ client-side/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Thoughts Today</h1>
+      </header>
+      <main>
+        <p>Welcome to my thoughts today. Here, I'll be sharing my musings, observations, and insights with you. Feel free to join the conversation!</p>
+        {/* Add more content here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
